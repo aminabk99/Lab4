2abd9704572a7f20273c1827cab3a40ec80874c9
Amina <aminabk@umich.edu>
2025-10-02 16:35:52 -0400

    First commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 public static void main(String[] args) {
 for(int i=1;i<=10;i++) {
-System.out.println("Hello World..."+i);
+System.out.println("Hello World. I am testing VS Code with Java and Github..."+i);
 }
 }
 }
